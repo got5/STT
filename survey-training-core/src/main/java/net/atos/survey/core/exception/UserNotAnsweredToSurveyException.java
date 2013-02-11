67a059b6150de106210eede5ecc94bd8d49114b6
Pierre Marot <pierre.marot@atos.net>
2013-02-11 18:20:11 +0100

    clean exception

--- survey-training-core/src/main/java/net/atos/survey/core/exception/UserNotAnsweredToSurveyException.java
+++ survey-training-core/src/main/java/net/atos/survey/core/exception/UserNotAnsweredToSurveyException.java
@@ -7,11 +7,7 @@ public class UserNotAnsweredToSurveyException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
-	public UserNotAnsweredToSurveyException(String message, Throwable cause,
-			boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
-	}
+	
 
 	public UserNotAnsweredToSurveyException(String message, Throwable cause) {
 		super(message, cause);
